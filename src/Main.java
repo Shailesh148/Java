8e8c9a65e67eac2dc34c18da579efcfd117077b6
siddharthathing <thingsiddhartha@gmail.com>
2022-10-13 18:01:49 +0545

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Siddhartha");
+import java.util.*;
+class EvenOdd{
+    public static void main(String[]abcd){
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value: ");
+        num = sc.nextInt();
+        if(num%2==0)
+        {
+            System.out.println("The given number is even number: " +num);
+        }
+        else
+            System.out.println("The given number is odd number: " +num);
     }
 }
