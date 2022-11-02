b021ac3d6adfb6e71a7e37a13f19e2e9b11627fe
Siddhartha Man Thing <62049027+siddharthathing@users.noreply.github.com>
2022-11-02 06:59:57 +0545

    Update SumPairs.java

--- src/SumPairs.java
+++ src/SumPairs.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SumPairs {
+import java.util.HashMap;
+
+public class SumPairs {
+    public static void main(String[] args) {
+        int []a = {2,45,7,3,5,1,8,9};
+        printSumPairs(a,10);
+    }
+    public static void printSumPairs(int []input, int k){
+        HashMap<Integer, Integer> pairs = new HashMap<Integer, Integer>();
+
+        for(int i=0;i<input.length;i++){
+
+            if(pairs.containsKey(input[i]))
+                System.out.println(input[i] +", "+ pairs.get(input[i]));
+            else
+                pairs.put(k-input[i], input[i]);
+        }
+
+    }
 }
